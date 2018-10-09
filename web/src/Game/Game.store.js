11b2dcdf1b42752524024915d9ee646d51c6f9cf
Mikael Edgren <mikael.edgren@whatwedo.se>
2018-10-09 23:33:37 +0200

    Generate table from games data

--- web/src/Game/Game.store.js
+++ web/src/Game/Game.store.js
@@ -1,27 +1,29 @@
 import {decorate, observable, action, computed} from 'mobx'
 import LogHelper from '../utils/LogHelper';
 
+/*eslint-disable*/
 const {logName, debug, log, error} = LogHelper.get('GameStore', LogHelper.STORE_TYPE);
+/*eslint-enable*/
 
 class GameStore {
 
   games = [];
 
-  setGames = (games) => {
-    debug(...logName, 'Setting games in store: ', games);
-    this.games = games;
-  };
-
   get isEmpty() {
     debug(this.games.length ? 'There are games in store' : 'There are no games in store');
     return this.games.length === 0;
   };
+
+  get playedGames(){
+    return this.games.filter(game => game.played);
+  }
 }
 
 decorate(GameStore, {
   games: observable,
   setGames: action,
   isEmpty: computed,
+  playedGames: computed,
 });
 const gameStore = new GameStore();
 export default gameStore;
---
+++ web/src/Table/Table.service.js
@@ -0,0 +1,62 @@
+import teamStore from '../Team/Team.store';
+import LogHelper from '../utils/LogHelper';
+import gameService from '../Game/Game.service';
+import _ from 'lodash';
+
+/*eslint-disable*/
+const {logName, debug, log, error} = LogHelper.get('TableService', LogHelper.SERVICE_TYPE);
+/*eslint-enable*/
+
+class TableService {
+
+  getTable = (teams, games) => {
+    log(...logName, 'Creating table!', teams, games);
+
+    let table = [];
+    teamStore.teams.forEach(team => {
+      table.push(this._createTableEntry(team, games));
+    });
+
+    table = _.orderBy(table, ['points', 'goalDifference', 'goalsScored'], ['desc', 'desc', 'desc']);
+    log(...logName, 'Table looks like this:', table);
+
+    return table;
+  };
+
+  _createTableEntry(team, games) {
+
+    const tableEntry = {
+      team: team,
+      goalsScored: 0,
+      goalsAgainst: 0,
+      goalDifference: 0,
+      points: 0,
+    };
+
+    const teamGames = gameService.getTeamGames(team, games);
+    debug(...logName, `--------- ${team.name} ---------`);
+
+    teamGames.forEach(game => {
+      const teamScore = game.homeTeam.code === team.code ? game.homeScore : game.awayScore;
+      const opponentScore = game.homeTeam.code === team.code ? game.awayScore : game.homeScore;
+
+      const points = teamScore > opponentScore ? (game.penaltyShots || game.overtime ? 2 : 3) : (game.penaltyShots || game.overtime ? 1 : 0);
+      tableEntry.points += points;
+      tableEntry.goalsScored += teamScore > opponentScore && game.penaltyShots ? teamScore - 1 : teamScore;
+      tableEntry.goalsAgainst += teamScore < opponentScore && game.penaltyShots ? opponentScore - 1 : opponentScore;
+
+      debug(...logName, '--------');
+      debug(...logName, `[${team.name}] team score is ${teamScore}, oppos score is ${opponentScore} ${game.penaltyShots ? '(on penalties)' : ''} ${game.overtime ? '(on overtime)' : ''}`);
+      debug(...logName, `[${team.name}] points are ${points}`);
+      debug(...logName, `[${team.name}] game is`, game);
+    });
+
+    tableEntry.goalDifference = tableEntry.goalsScored - tableEntry.goalsAgainst;
+
+    debug(...logName, `[${team.name}] table entry: `, tableEntry);
+    return tableEntry;
+  }
+}
+
+const tableService = new TableService();
+export default tableService;
